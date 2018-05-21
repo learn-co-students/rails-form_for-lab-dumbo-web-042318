aace9d5d799a7ddc54ea3d7b2d71e2c8675be0b6
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-21 17:35:27 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :students, except: [:index, :destroy]
+  resources :school_classes, except: [:index, :destroy]
 end
