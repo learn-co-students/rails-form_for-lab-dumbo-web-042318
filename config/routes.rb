aa5a584f58efdf947fb5e1ccae68ebed7d06a1f2
github username <github email address>
2018-05-22 16:22:02 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,11 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :students, only: [:new, :show, :create, :edit]
+
+    patch 'students/:id', to: 'students#update'
+
+  resources :school_classes, only: [:new, :show, :create, :edit]
+
+    patch 'school_classes/:id', to: 'school_classes#update'
 end
