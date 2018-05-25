bcd87e4d2c86691bb734114e7554f26f65b012e2
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-05-25 14:52:21 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -13,7 +13,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(paramsrequire(:school_class).permit(:title, :room_number))
+    @school_class = SchoolClass.new(school_class_params)
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
@@ -25,14 +25,16 @@ class SchoolClassesController < ApplicationController
 
   def update
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(school_class_params(:title, :room_number))
+    @school_class.update(school_class_params)
     redirect_to school_class_path(@school_class)
   end
 
-  def school_class_params(*args)
-    params.require(:school_class).permit(*args)
+  private
+
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
   end
 
-  
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-    resources :school_classes, only: [:new, :create, :show, :edit]
-    resources :students, only: [:new, :create, :show, :edit]
+    resources :school_classes, only: [:new, :create, :show, :edit, :update]
+    resources :students, except: :destroy
 end
